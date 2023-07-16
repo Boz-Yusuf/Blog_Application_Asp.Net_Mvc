592884d1fb475caacc3308d0b0f7445be23ef88d
Boz-Yusuf <boz35yusuf@gmail.com>
2023-07-16 12:57:35 +0300

    Repositories were commited

---
+++ Blog.Core/Repository/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Repository
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+        public int UserCredentialsId { get; set; }
+        public UserCredentials UserCredentials { get; set; }
+    }
+}
